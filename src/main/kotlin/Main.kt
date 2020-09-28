95a628a1d3dcec1a80cff1629b61672830c5b455
Jekson353 <admin@room13.ru>
2020-09-29 00:55:03 +0700

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,61 @@
+package ru.wall
+import java.util.*
+
+
+fun main() {
+    val calendar = Calendar.getInstance()
+    val timestamp = calendar.timeInMillis
+    val attachment: Attachment = AudioAttachment(AttachmentType.AUDIO, Audio(id = 1, ownerId = 10, albumId = 15, userId = 100))
+
+    val post = Post(
+            id = 1,
+            ownerId = 15,
+            fromId = 25,
+            createdBy = 1,
+            date = timestamp,
+            text = "Новый пост 1",
+            replyPostId = 10,
+            comments = "",
+            copyright = "",
+            attachments = attachment,
+            likes = 0,
+            views = 0,
+            replyOwnerId = null,
+            postType = "",
+            signerId = 0,
+            geo = null,
+            postSource = null,
+            reposts = null,
+    )
+    println("Первый пост: $post")
+    WallService.add(post)
+    val twoPost = Post(
+            id = 2,
+            ownerId = 15,
+            fromId = 25,
+            createdBy = 1,
+            date = timestamp,
+            text = "Новый пост 2",
+            replyPostId = 10,
+            comments = "",
+            copyright = "",
+            attachments = attachment,
+            likes = 0,
+            views = 0,
+            postType = "",
+            signerId = 0,
+            replyOwnerId = null,
+            geo = null,
+            postSource = null,
+            reposts = null,
+    )
+    println("Второй пост: $twoPost")
+    WallService.add(twoPost)
+    val result1 = WallService.getPost(id = 2)
+    println("Пост до изменения: $result1")
+    val updatedPost = twoPost.copy(likes = 10)
+    WallService.update(updatedPost)
+
+    val result2 = WallService.getPost(id = 2)
+    println("Пост после изменения: $result2")
+}
